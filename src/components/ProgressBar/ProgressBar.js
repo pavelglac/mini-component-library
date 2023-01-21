8ea2c1648f82fc2c15c46f669287974d69534522
Pavel Glac <pavelglac@gmail.com>
2023-01-21 12:01:26 +0000

    progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,68 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  let Component;
+
+  switch (size) {
+    case 'large':
+      Component = LargeProgressBar;
+      break;
+    case 'medium':
+      Component = MediumProgressBar;
+      break;
+    case 'small':
+      Component = SmallProgressBar;
+      break;
+  }
+  return (<>
+    <VisuallyHidden><label for="progress">File progress:</label></VisuallyHidden>
+    <Component 
+      id="progress"
+      aria-label={value !== 100 ? `Progress: ${value}%` : 'Complete'}
+      role="progressbar"
+      max="100"
+      value={value}
+    >
+      {value} %
+    </Component>
+  </>)
 };
+
+const ProgressBase = styled.progress`
+  border-radius: 8px;
+  -webkit-appearance: none;
+  overflow: hidden;
+  width: 100%;
+
+  &::-webkit-progress-bar {
+    background-color: ${COLORS.transparentGray15};
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  };
+
+  &::-webkit-progress-value {
+    background-color: ${COLORS.primary};
+    border-radius: 8px 0 0 8px;
+
+    ${props => props.value === 100 && `
+      border-radius: 8px; 
+    `}
+  }
+`
+
+const SmallProgressBar = styled(ProgressBase)`
+  height: 8px;
+`
+
+const MediumProgressBar = styled(ProgressBase)`
+  height: 12px;
+`
+
+const LargeProgressBar = styled(ProgressBase)`
+  height: 24px;
+
+  &::-webkit-progress-bar {
+    padding: 4px;
+  };
+`
 
 export default ProgressBar;
