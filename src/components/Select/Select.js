3ba02a2945083a066517e859f233a9f4407cea07
Pavel Glac <pavelglac@gmail.com>
2023-01-22 16:39:49 +0000

    select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -3,16 +3,57 @@ import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import VisuallyHidden from '../VisuallyHidden';
 
-const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
-
-  return (
-    <select value={value} onChange={onChange}>
+const Select = ({ label, value, onChange, children }) => (
+  <SelectWrapper>
+    <VisuallyHidden>
+      <label for="select">{label}</label>
+    </VisuallyHidden>
+    <SelectElement name='select' value={value} onChange={onChange}>
       {children}
-    </select>
-  );
-};
+    </SelectElement>
+    <DropDownIcon />
+  </SelectWrapper>
+);
+
+
+const SelectElement = styled.select`
+  appearance: none;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  position: relative;
+  padding: 12px 48px 12px 12px;
+  color: ${COLORS.gray700};
+  border: 2px solid transparent;
+  transition: all 200ms;
+  outline: none;
+  font-size: 1rem;
+  line-height: 1;
+
+  &:focus {
+    border-color: ${COLORS.primary};
+  }
+`
+
+const DropDownIcon = styled(Icon).attrs({ id: 'chevron-down', size: 22, strokeWidth: 2 })`
+  position: absolute;
+  right: 14px;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  color: ${COLORS.gray700};
+  transition: all 200ms;
+
+`
+
+const SelectWrapper = styled.div`
+  position: relative;
+  width: max-content;
+
+  &:hover ${DropDownIcon}, &:hover ${SelectElement} {
+    color: ${COLORS.black};
+  }
+`
 
 export default Select;
